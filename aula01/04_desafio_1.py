80b6689169b094c29e6d078152a4349329fa98ba
RaquelGomes25 <92063008+RaquelGomes25@users.noreply.github.com>
2022-04-25 13:44:11 -0300

    Aula 01 atualizada

--- aula01/04_desafio_1.py
+++ aula01/04_desafio_1.py
@@ -1,3 +1,8 @@
 # 1. Peça ao seu usuário para inserir 3 dados: nome, idade e cidade.
+nome = input ("Qual o seu nome? ")
+idade = input ("Quantos anos você tem? ")
+cidade = input ("Você é de qual cidade? ")
+print("Meu nome é " + nome +" tenho " + idade +" anos, e moro em "+ cidade)
 
 # 2. Imprima no console uma única mensagem desejando boas vindas ao usuário.
+print("Seja bem vinda "+ nome)
--- aula01/07_desafio_2.py
+++ aula01/07_desafio_2.py
@@ -1,8 +1,13 @@
 # 1. Pergunte ao usuário em qual cidade e o estado em que ele(a) mora
+cidade = input ("Você mora em qual cidade? ")
+estado = input ("Você mora em qual estado? ")
 
 # 2. Imprima uma mensagem diga a cidade em que o usuário mora. 
 #    O nome da cidade deve estar todo em letras maiúsculas.
+cidade_maiscula = cidade.upper()
+print("Você mora em" , cidade_maiscula)
 
 # 3. Imprima uma mensagem diga o estado em que o usuário mora. 
 #    O nome do estado deve estar todo em letras maiúsculas.
 #    Utilize uma forma de formatação de string diferente da que foi usada no item 2.
+print("Você mora na " + estado.lower())
